8bc9f7d589c5eb2b80a61918baa384ce51067edb
matheshar <silikathembelani@gmail.com>
2016-02-08 12:16:59 +0200

    Fixed test

--- rest/src/test/java/bookmarks/BookmarkRestControllerTest.java
+++ rest/src/test/java/bookmarks/BookmarkRestControllerTest.java
@@ -87,7 +87,6 @@ public class BookmarkRestControllerTest {
 
     @Test
     public void userNotFound() throws Exception {
-        Assert.assertTrue(false);
         mockMvc.perform(post("/george/bookmarks/")
                 .content(this.json(new Bookmark()))
                 .contentType(contentType))
